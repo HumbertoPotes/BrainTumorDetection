43da03da301b9309181239bac1085fd3b07bfe1d
Chris Kinder <cbkinder96@yahoo.com>
2025-04-24 20:43:43 -0400

    built dataset class and data loader in the training loop

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 from datasets import load_dataset
+from PIL import Image
+from dataset import BrainTumorDataset
+from torch.utils.data import DataLoader
 import torch
 
 def train():
@@ -11,11 +14,22 @@ def train():
         device = torch.device("cpu")
     print(f"Using device: {device}")
 
-    # load the dataset
+    # load the dataset from Hugging Face Hub
     dataset = load_dataset("dwb2023/brain-tumor-image-dataset-semantic-segmentation")
     print(dataset)
-    for image in dataset["test"]:
-        print(len(image["segmentation"][0]))
+
+    # load the training dataset
+    train_dataset = BrainTumorDataset(dataset['train'])
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+    for images, masks in train_loader:
+        # move images to the device
+        images = images.to(device)
+        masks = masks.to(device)
+
+        # print the shape of the images tensor
+        print(f"Images shape: {images.shape}")
+        print(f"Masks shape: {masks.shape}")
 
 if __name__ == "__main__":
     train()
